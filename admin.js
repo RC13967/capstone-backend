ea7426b5d4de2aaa1200bd1d79569458c91d62f7
ranjith cheerla <ranjithch137@gmail.com>
2022-08-29 17:12:51 +0530

    client details added

--- admin.js
+++ admin.js
@@ -60,8 +60,8 @@ router1.post("/UserSignUp", async (request, response) => {
       // type: 'OAuth2',
       user: process.env.MAIL_USERNAME,
       pass: process.env.MAIL_PASSWORD,
-      // clientId: process.env.OAUTH_CLIENTID,
-      // clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
       // refreshToken: process.env.OAUTH_REFRESH_TOKEN
     }
   });
@@ -114,8 +114,8 @@ router1.post("/forgotUser", async (request, response) => {
         // type: 'OAuth2',
         user: process.env.MAIL_USERNAME,
         pass: process.env.MAIL_PASSWORD,
-        // clientId: process.env.OAUTH_CLIENTID,
-        // clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
         // refreshToken: process.env.OAUTH_REFRESH_TOKEN
       }
     });
