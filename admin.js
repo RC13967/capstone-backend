db10a5d63fc4b989822eba3ed6355c1253b80587
ranjith cheerla <ranjithch137@gmail.com>
2022-08-29 17:08:04 +0530

    oauth2 removed

--- admin.js
+++ admin.js
@@ -57,12 +57,12 @@ router1.post("/UserSignUp", async (request, response) => {
   let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      type: 'OAuth2',
+      // type: 'OAuth2',
       user: process.env.MAIL_USERNAME,
       pass: process.env.MAIL_PASSWORD,
-      clientId: process.env.OAUTH_CLIENTID,
-      clientSecret: process.env.OAUTH_CLIENT_SECRET,
-      refreshToken: process.env.OAUTH_REFRESH_TOKEN
+      // clientId: process.env.OAUTH_CLIENTID,
+      // clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      // refreshToken: process.env.OAUTH_REFRESH_TOKEN
     }
   });
   let mailOptions = {
@@ -111,12 +111,12 @@ router1.post("/forgotUser", async (request, response) => {
     let transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        type: 'OAuth2',
+        // type: 'OAuth2',
         user: process.env.MAIL_USERNAME,
         pass: process.env.MAIL_PASSWORD,
-        clientId: process.env.OAUTH_CLIENTID,
-        clientSecret: process.env.OAUTH_CLIENT_SECRET,
-        refreshToken: process.env.OAUTH_REFRESH_TOKEN
+        // clientId: process.env.OAUTH_CLIENTID,
+        // clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        // refreshToken: process.env.OAUTH_REFRESH_TOKEN
       }
     });
     let mailOptions = {
